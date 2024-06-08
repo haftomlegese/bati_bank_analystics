73b06295aedccada826239314b446be7999652ff
haftomlegese <hafnium2@gmail.com>
2024-06-08 14:46:43 +0300

    credit risk modeling added

---
+++ notebooks/cat_breaks_20240607_193452.py
@@ -0,0 +1,5 @@
+breaks_list={
+'ChannelId': ['ChannelId_5%,%ChannelId_3','ChannelId_2%,%ChannelId_1'], 
+'ProductCategory': ['transport%,%utility_bill%,%airtime%,%data_bundles','financial_services%,%movies%,%ticket%,%tv'], 
+'ProductId': ['ProductId_23%,%ProductId_16%,%ProductId_7%,%ProductId_14%,%ProductId_27%,%ProductId_10%,%ProductId_1','ProductId_15%,%ProductId_13%,%ProductId_5%,%ProductId_21%,%ProductId_4%,%ProductId_11','ProductId_24%,%ProductId_6%,%ProductId_22','ProductId_3%,%ProductId_2%,%ProductId_8%,%ProductId_20%,%ProductId_19%,%ProductId_9'], 
+'ProviderId': ['ProviderId_2%,%ProviderId_5%,%ProviderId_3%,%ProviderId_1','ProviderId_4','ProviderId_6']}
